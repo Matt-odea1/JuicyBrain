35f89c8e4d67bf0311464aecc2eaf50fa8510db2
Matt O'Dea <z5413887@ad.unsw.edu.au>
2023-04-05 12:51:29 +1000

    Nothing really changed, new button file

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View} from "react-native";
+
+import Button from './components/Button';
+import ImageViewer from './components/ImageViewer';
+
+const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style ={{ color: '#fff'}}>
-        This guy is a big loser!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#25292e',
+  // Styles that are unchanged from previous step are hidden for brevity. 
+  footerContainer: {
+    flex: 1 / 3,
     alignItems: 'center',
-    justifyContent: 'center',
   },
 });
